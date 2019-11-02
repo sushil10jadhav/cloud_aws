83d51de5f9dcab84c8f3499a222ef9c3282eee2a
sushil10.jadhav@gmail.com <sushil10.jadhav@gmail.com>
2019-11-02 18:58:20 +0530

    added ansible files

---
+++ ansible/bootStrap_AnsibleHosts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Bootstrap script to snip up ubuntu instances for ansible works
+apt-get update -y
+useradd -mG admin sushil
+echo "sushil:sushil123"|chpasswd
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+service sshd restart
+apt install python -y
