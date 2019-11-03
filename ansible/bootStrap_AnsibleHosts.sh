5709f0f2f2be5235ff880401d7e153cbe4927699
sushil10.jadhav@gmail.com <sushil10.jadhav@gmail.com>
2019-11-04 00:57:30 +0530

    few more

--- ansible/bootStrap_AnsibleHosts.sh
+++ ansible/bootStrap_AnsibleHosts.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Bootstrap script to snip up ubuntu instances for ansible works
 apt-get update -y
+apt-get upgrade -y
 useradd -mG admin sushil
 echo "sushil:sushil123"|chpasswd
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 service sshd restart
 apt install python -y
+apt install sshpass
